f75b2aa62206582b294828af7e671dc471731611
Ryosuke Izumi <ryosuke.izumi62@gmail.com>
2021-12-12 11:11:55 +0900

    add trpc server

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import express from 'express';
 import cors from 'cors';
-import { createRouter } from './router';
+import { appRouter, createContext } from './router';
+import * as trpcExpress from '@trpc/server/adapters/express';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const port = 3000;
+const port = 3001;
 
-app.use('/', createRouter());
+app.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
+
+app.get('/', (_req, res) => res.send('hello'));
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/`);
--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,38 @@
-import { Router } from 'express';
+import * as trpc from '@trpc/server';
+import { z } from 'zod';
+import * as trpcExpress from '@trpc/server/adapters/express';
 
-export const createRouter = () => {
-  const router = Router();
+export const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => {
+  const getAuthorization = () => {
+    return req.headers.authorization || '';
+  };
 
-  router.get('/healthchack', (req, res) => {
-    res.status(200).send({ message: 'Alive' });
-  });
-
-  return router;
+  return {
+    req,
+    res,
+    authorization: getAuthorization(),
+  };
 };
+type Context = trpc.inferAsyncReturnType<typeof createContext>;
+
+const createRouter = () => {
+  return trpc.router<Context>();
+};
+
+export const appRouter = createRouter().query('hello', {
+  input: z
+    .object({
+      text: z.string().nullish(),
+    })
+    .nullish(),
+  resolve({ input }) {
+    return {
+      greeting: `hello ${input?.text ?? 'world'}`,
+    };
+  },
+});
+
+export type AppRouter = typeof appRouter;
